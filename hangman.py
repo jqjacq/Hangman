197eae177147b82187ba5c3541302da53c5efefc
Jacqueline Xu <xujacque@gmail.com>
2023-11-16 14:03:01 -0500

    function to start game, play again, display progress & guesses

--- hangman.py
+++ hangman.py
@@ -1,11 +1,8 @@
-from draw_hangman import draw_hangman as hangman
-from word_selection import word_selection as random_word
+from draw_hangman import draw_hangman as draw_hangman
+from word_selection import word_selection as hidden_word
 import string
 
 max_incorrect_guesses = 6
-
-print(random_word())
-hangman(0)
 
 # Get user input
 def get_user_input(letters_guessed):
@@ -24,20 +21,68 @@ def validate_input(user_input, letters_guessed):
 
 # Display the word with underscores for letters not yet guessed
 def display_word(word, letters_guessed):
-    output_letters = []
+    correct_letters = ''
     for letter in word:
         if letter in letters_guessed:
-            output_letters.append(letter)
+            correct_letters += letter
+            # correct_letters.append(letter)
         else:
-            output_letters.append("_")
-    return " ".join(output_letters)
+            correct_letters += "_ "
+            # correct_letters.append("_")
+    return correct_letters
+            #" ".join(correct_letters)
 
 # Display the letters that have been guessed
 def join_letters_guessed(letters_guessed):
     return " ".join(sorted(letters_guessed))
 
-# Check if the game is over
-# def game_over(wrong_guesses, target_word, letters_guessed):
-#     return (
-#         wrong_guesses == max_incorrect_guesses or set(target_word) <= letters_guessed:
-#     ) 
+# Display the progress
+def display_progress(hidden_word, letters_guessed, remaining_guesses):
+    word_progress = ''.join(letter if letter in letters_guessed else "_" for letter in hidden_word)
+
+    print(f"Word to guess: {word_progress}")
+    print(f"Letters guessed: {join_letters_guessed(letters_guessed)}")
+    print(f"Remaining guesses: {remaining_guesses}")
+
+
+# Start and loop through the game
+def hangman(hidden_word):
+    remaining_guesses = max_incorrect_guesses
+    letters_guessed = []
+
+    while remaining_guesses > 0:
+        display_progress(hidden_word, letters_guessed, remaining_guesses) #display progress
+        draw_hangman(6 - remaining_guesses) #display hangman
+        guess = get_user_input(letters_guessed) #get user input
+        letters_guessed.append(guess)
+        if guess in hidden_word:
+            print("Correct!")
+        else:
+            print("Incorrect!")
+            remaining_guesses -= 1
+
+        if remaining_guesses == 0:
+            draw_hangman(max_incorrect_guesses - remaining_guesses)
+            print("You ran out of tries! The word was " + hidden_word + ".")
+        
+        # Check if all letters have been guessed
+        if "_" not in display_word(hidden_word, letters_guessed):
+            print("Congratulations! You guessed the word!")
+            break
+
+    play_again()
+
+# Ask if the user wants to play again
+def play_again():
+    while True:
+        play_again = input("Do you want to play again? (Y/N) ").lower()
+        if play_again == "y":
+            hangman(hidden_word())
+        elif play_again == "n":
+            print("Thanks for playing!")
+            break
+        else:
+            print("Invalid input. Please enter Y or N.")
+
+# Start the game
+hangman(hidden_word())
